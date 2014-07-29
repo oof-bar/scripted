6b4815a816b6008126cd5f8636b111cbb32b927c
August W. Miller <hello@gusmiller.com>
2014-07-28 18:41:57 -0700

    Paginated blog posts more carefully

--- wp-content/themes/scripted/partials/pagination.php
+++ wp-content/themes/scripted/partials/pagination.php
@@ -1,1 +1,19 @@
-<?= posts_nav_link() ?>
+<? if ( get_previous_posts_link() || get_next_posts_link() ) { ?>
+
+  <div class="pagination">
+
+    <? if ( get_previous_posts_link() ) { ?>
+      <div class="previous">
+        <? previous_posts_link('Newer'); ?>
+      </div>
+    <? } ?>
+
+    <? if ( get_next_posts_link() ) { ?>
+      <div class="next">
+        <? next_posts_link('Older'); ?>
+      </div>
+    <? } ?>
+    
+  </div>
+
+<? } ?>

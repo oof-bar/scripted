3787572d16317a6ac483935b747d13982935651f
savannahjulian <savannah.julian@gmail.com>
2014-08-02 14:23:55 -0700

    Working on style guide

--- wp-content/themes/scripted/style-guide.php
+++ wp-content/themes/scripted/style-guide.php
@@ -9,6 +9,23 @@
       <h3>Header Level 3</h3>
       <h4>Header Level 4</h4>
       <h5>Header Level 5</h5>
+    </div>
+  </div>
+</section>
+
+<section>
+  <div class="wrapper">
+    <div class="column col-8 push-2">
+      <div class="data">
+        Data: 1 out of 10
+      </div>
+    </div>
+  </div>
+</section>
+
+<section>
+  <div class="wrapper">
+    <div class="column col-8 push-2">
       <p class="large">
         ScriptEd empowers students to be creators of technology by offering computer programming classes in under resourced high schools. Our courses are held twice weekly in our partner schools and taught by software developers on a volunteer basis. During the summer, we link students to internship opportunities where they apply their coding skills.
       </p>
@@ -19,16 +36,103 @@
         ScriptEd empowers students to be creators of technology by offering computer programming classes in under resourced high schools. Our courses are held twice weekly in our partner schools and taught by software developers on a volunteer basis. During the summer, we link students to internship opportunities where they apply their coding skills.
       </p>
       <p>
-        ScriptEd empowers students to be creators of technology by offering computer programming classes in under resourced high schools. Our courses are held twice weekly in our partner schools and taught by software developers on a volunteer basis. During the summer, we link students to internship opportunities where they apply their coding skills.
+        Default Paragraph: ScriptEd empowers students to be creators of technology by offering computer programming classes in under resourced high schools. Our courses are held twice weekly in our partner schools and taught by software developers on a volunteer basis. During the summer, we link students to internship opportunities where they apply their coding skills.
       </p>
       <p>
         ScriptEd empowers students to be creators of technology by offering computer programming classes in under resourced high schools. Our courses are held twice weekly in our partner schools and taught by software developers on a volunteer basis. During the summer, we link students to internship opportunities where they apply their coding skills.
       </p>
+    </div>
+  </div>
+</section>
 
-      <section class="gray-bg">
-        ScriptEd empowers students to be creators of technology by offering computer programming classes in under resourced high schools.
-      </section>
-      
+<section class="testimonials">
+  <div class="wrapper">
+    <div class="column col-8 push-2">
+      <div class="quote serif">
+        Quote Testimonial: ScriptEd empowers students to be creators of technology by offering computer programming classes in under resourced high schools.
+      </div>
+      <div class="attribution caps sans blue">Savannah Julian</div>
+    </div>
+  </div>
+</section>
+
+<section class="featured-quote">
+  <div class="wrapper">
+    <div class="column col-8 push-2">
+      <div class="blue-bg padded text-center white">
+        <div class="quote">
+          Quote Quote-Box: ScriptEd empowers students to be creators of technology by offering computer programming classes in under resourced high schools.
+        </div>
+        <div class="attribution">Savannah Julian</div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="gray-bg">
+  <div class="wrapper">
+    <div class="column col-8 push-2">
+      section.gray-bg
+    </div>
+  </div>
+</section>
+
+<br>
+
+<section class="border">
+  <div class="wrapper">
+    <div class="column col-8 push-2">
+      section.border
+    </div>
+  </div>
+</section>
+
+<br>
+
+<section class="gray-bg expanded">
+  <div class="wrapper">
+    <div class="column col-8 push-2">
+      section.gray-bg.expanded
+    </div>
+  </div>
+</section>
+
+<section>
+  <div class="wrapper">
+    <div class="column col-8 push-2">
+      <div class="button orange">
+        orange
+      </div>
+
+      <div class="button blue">
+        blue
+      </div>
+
+      <br>
+
+      <div class="button orange outline">
+        orange
+      </div>
+
+      <div class="button blue outline">
+        blue
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="black-bg">
+  <div class="wrapper">
+    <div class="column col-8 push-2">
+      <div class="button white">
+        white
+      </div>
+
+      <br>
+
+      <div class="button white outline">
+        white
+      </div>
     </div>
   </div>
 </section>

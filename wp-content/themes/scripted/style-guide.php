9b885fa3576e932fa9015e7f03ddc1b9fc318cbc
savannahjulian <savannah.julian@gmail.com>
2014-08-01 21:51:41 -0700

    Adding Style Guide

---
+++ wp-content/themes/scripted/style-guide.php
@@ -0,0 +1,12 @@
+<? /* Template Name: Style Guide */ ?>
+<? get_header() ?>
+
+<section class="style-guide">
+  <div class="wrapper">
+    <div class="column col-8 push-2">
+      <h1>asdf</h1>
+    </div>
+  </div>
+</section>
+
+<? get_footer() ?>

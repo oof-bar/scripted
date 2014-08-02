6aef2c68c549a3936b2af8265267ad46476aa2a1
August W. Miller <hello@gusmiller.com>
2014-08-01 23:00:09 -0700

    Search, press, media abstractions

---
+++ wp-content/themes/scripted/archive-se_resource.php
@@ -0,0 +1,25 @@
+<? get_header(); ?>
+<? $page_title = post_type_archive_title(null, false) ?>
+<? include get_partial('hero') ?>
+
+<section class="main resource-archive">
+  <div class="wrapper">
+
+    <div class="column col-3 tablet-quarter">
+      <? include get_partial('blog-navigation') ?>
+    </div>
+
+    <div class="column col-7 tablet-three-quarters">
+      <? if ( have_posts() ) { ?>
+        <!-- Starting post -->
+        <? while ( have_posts() ) { the_post(); ?>
+          <? get_embed( get_post_type() ); ?>
+        <? } ?>
+      <? } ?>
+      <? include get_partial('pagination') ?>
+    </div>
+    
+  </div>
+</section>
+
+<? get_footer(); ?>
--- wp-content/themes/scripted/search.php
+++ wp-content/themes/scripted/search.php
@@ -1,15 +1,10 @@
-<? /* Search */ ?>
 <? get_header(); ?>
-<h1>Search</h1>
-<h2>We found <?= speak_number(count($posts)) ?> <?= _n( 'thing', 'things', count($posts) ) ?> for &ldquo;<span class="search-term"><?= get_search_query() ?></span>&rdquo;</h2>
+<? include get_partial('hero-search') ?>
 
 <section class="main search">
   <div class="wrapper">
-    <div class="column col-3 tablet-quarter">
-      <? include get_partial('blog-navigation') ?>
-    </div>
 
-    <div class="column col-7 tablet-three-quarters">
+    <div class="column col-7 push-3 tablet-three-quarters">
       <? if ( have_posts() ) { ?>
         <!-- Starting post -->
         <? while ( have_posts() ) { the_post(); ?>
@@ -18,6 +13,7 @@
       <? } ?>
       <? include get_partial('pagination') ?>
     </div>
+
   </div>
 </section>
 

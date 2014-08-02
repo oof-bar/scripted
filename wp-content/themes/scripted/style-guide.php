79959a9d7881f912eeb0205ad4ff97f3b7f7920e
savannahjulian <savannah.julian@gmail.com>
2014-08-01 23:19:57 -0700

    header sizes

--- wp-content/themes/scripted/style-guide.php
+++ wp-content/themes/scripted/style-guide.php
@@ -4,9 +4,12 @@
 <section class="style-guide">
   <div class="wrapper">
     <div class="column col-8 push-2">
-      <h1>asdf</h1>
-      <h2>asdf</h2>
-      <h3>asdf</h3>
+      <h1>Header Level 1</h1>
+      <h2>Header Level 2</h2>
+      <h3>Header Level 3</h3>
+      <h4>Header Level 4</h4>
+      <h5>Header Level 5</h5>
+      <h6>Header Level 6</h6>
     </div>
   </div>
 </section>

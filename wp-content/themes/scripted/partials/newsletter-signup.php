75323845431df99a5779582f65d5d9d1d8d8d3f6
August W. Miller <hello@gusmiller.com>
2014-08-02 13:02:55 -0700

    Newsletter signup submit is a button

--- wp-content/themes/scripted/partials/newsletter-signup.php
+++ wp-content/themes/scripted/partials/newsletter-signup.php
@@ -2,6 +2,6 @@
   <? $email_nonce = wp_create_nonce('email_signup_nonce'); ?>
   <input type="email" name="se-email-subscriber" id="se-email-subscriber" />
   <input type="hidden" name="se-email-nonce" value="<?= $email_nonce ?>" id="se-email-nonce"/>
-  <input type="submit" value="Sign me up!" />
+  <input class="button" type="submit" value="Sign me up!" />
 </form>
 <div id="email-response"></div>

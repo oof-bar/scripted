fb5eb4738c62b86edc624fb49ef7859033f57213
August W. Miller <hello@gusmiller.com>
2014-08-03 20:35:23 -0700

    Google Fonts loaded with https

--- wp-content/themes/scripted/header.php
+++ wp-content/themes/scripted/header.php
@@ -5,7 +5,7 @@
     <meta name="tags" content="<?= se_meta_tags() ?>">
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
     <!-- <link rel="stylesheet" href="//code.cdn.mozilla.net/fonts/fira.css"> -->
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700|Roboto:100,300,400,500,700,900,100italic,300italic,400italic,500italic,700italic,900italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700|Roboto:100,300,400,500,700,900,100italic,300italic,400italic,500italic,700italic,900italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
     <? wp_head(); ?>
   </head>
 

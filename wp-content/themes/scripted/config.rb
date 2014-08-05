95348b1fbb872af5fc0178f24e834a6bf66192e9
August W. Miller <hello@gusmiller.com>
2014-08-04 21:58:18 -0700

    New config.rb for Compass

--- wp-content/themes/scripted/config.rb
+++ wp-content/themes/scripted/config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "/wp-content/themes/scripted/"
 css_dir = "stylesheets"
 sass_dir = "scss"
 images_dir = "images"

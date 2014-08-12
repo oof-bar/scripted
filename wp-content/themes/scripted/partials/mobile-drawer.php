32d37cac90b216d4762ba484102e43feccaddea0
August W. Miller <hello@gusmiller.com>
2014-08-12 01:28:54 -0700

    Added mobile drawer
    Along with javascript toggled, and a newly registered menu.

---
+++ wp-content/themes/scripted/partials/mobile-drawer.php
@@ -0,0 +1,12 @@
+<section class="mobile-drawer">
+  <div class="drawer-header">
+    <div class="title"><?= bloginfo('title') ?></div>
+  </div>
+  <? wp_nav_menu( array(
+    'theme_location' => 'mobile',
+    'container_class' => 'mobile-nav'
+  )) ?>
+  <div class="drawer-footer">
+    <div class="wrap small"><?= se_option('np_notice') ?></div>
+  </div>
+</section>

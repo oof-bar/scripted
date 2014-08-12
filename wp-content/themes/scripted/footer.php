afb97f1a7fea83b4bca30cc80205061de4d33cc4
August W. Miller <hello@gusmiller.com>
2014-08-12 01:38:26 -0700

    Add wrapper around content that needs to slide

--- wp-content/themes/scripted/footer.php
+++ wp-content/themes/scripted/footer.php
@@ -1,32 +1,34 @@
-    <section class="footer border">
-      <div class="wrapper">
+      <section class="footer border">
+        <div class="wrapper">
 
-        <div class="column col-4 tablet-half mobile-full">
-          <div class="wordmark bold serif text-orange">
-            <?= bloginfo('title') ?>
+          <div class="column col-4 tablet-half mobile-full">
+            <div class="wordmark bold serif text-orange">
+              <?= bloginfo('title') ?>
+            </div>
+            <div class="non-profit-note">
+              <?= se_option('np_notice') ?>
+            </div>
+            <? if ( se_option("mailchimp_list") ) include "partials/newsletter-signup.php"; ?>
           </div>
-          <div class="non-profit-note">
-            <?= se_option('np_notice') ?>
-          </div>
-          <? if ( se_option("mailchimp_list") ) include "partials/newsletter-signup.php"; ?>
-        </div>
 
-        <div class="column col-2 push-1 tablet-half mobile-full">
-          <span class="caps semi-bold small">Explore</span> 
-          <div class="add-margin-top caps bold small link-grey">
-            <? wp_nav_menu(array(
-              'theme_location' => 'footer',
-              'container_class' => 'footer-nav'
-            )); ?>
+          <div class="column col-2 push-1 tablet-half mobile-full">
+            <span class="caps semi-bold small">Explore</span> 
+            <div class="add-margin-top caps bold small link-grey">
+              <? wp_nav_menu(array(
+                'theme_location' => 'footer',
+                'container_class' => 'footer-nav'
+              )); ?>
+            </div>
           </div>
-        </div>
-        
-        <div class="column col-5 tablet-full caps add-margin-bottom semi-bold small">
-          Sponsors
-        </div>
+          
+          <div class="column col-5 tablet-full caps add-margin-bottom semi-bold small">
+            Sponsors
+          </div>
 
-      </div>
-    </section>
+        </div>
+      </section>
+
+    </div>
 
     <? include get_partial('analytics'); ?>
     <? include get_partial('typekit'); ?>

07d57c986890abb2bff4f8c71e35f3792e2cd1f6
August W. Miller <hello@gusmiller.com>
2014-08-14 01:09:58 -0700

    Better post-meta section

--- wp-content/themes/scripted/partials/post-meta.php
+++ wp-content/themes/scripted/partials/post-meta.php
@@ -1,10 +1,10 @@
 <div class="post-meta">
   <div class="meta-item author">
-    <h6>Author</h6>
+    <h6 class="small caps text-grey-mid">Author</h6>
     <? the_author() ?>
   </div>
   <div class="meta-item post-date">
-    <h6>Date</h6>
-    <? the_date() ?> at <? the_time() ?>
+    <h6 class="small caps text-grey-mid">Date</h6>
+    <?= the_date() ?>
   </div>
 </div>

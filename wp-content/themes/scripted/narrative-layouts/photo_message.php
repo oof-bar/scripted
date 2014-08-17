e9a58f5c006d85ccc933a386aec5e6ca3b0c3cc5
August W. Miller <hello@gusmiller.com>
2014-08-16 21:23:38 -0700

    Icon optional, added attribution

--- wp-content/themes/scripted/narrative-layouts/photo_message.php
+++ wp-content/themes/scripted/narrative-layouts/photo_message.php
@@ -2,15 +2,15 @@
 
 <section class="narrative-section message expanded">
 
-    <div class="wrapper text">
+    <div class="wrapper">
 
       <div class="column col-8">
 
-        <? if ( $section['header'] ) { ?>
+        <? if ( !empty($section['header']) ) { ?>
           <h4 class="text-blue"><?= $section['header'] ?></h4>
         <? } ?>
 
-        <? if ( $section['message'] ) { ?>
+        <? if ( !empty($section['message']) ) { ?>
           <div class="greedy lede large">
             <?= $section['message'] ?>
           </div>
@@ -19,9 +19,11 @@
       </div>
       
       <div class="column col-4">
-        <figure>
-          <img src="<?= $section['illustration']['sizes']['medium'] ?>" alt="<?= $quote['header'] ?>" />
-        </figure>
+        <? if ( !empty($section['illustration']) ) { ?>
+          <figure>
+            <img src="<?= $section['illustration']['sizes']['medium'] ?>" alt="<?= $quote['header'] ?>" />
+          </figure>
+        <? } ?>
       </div>
 
     </div>
--- wp-content/themes/scripted/narrative-layouts/point_of_pride.php
+++ wp-content/themes/scripted/narrative-layouts/point_of_pride.php
@@ -1,20 +1,25 @@
 <? /* Point of Interest */ ?>
 
-<section class="narrative-section pride black">
+<section class="narrative-section pride black<?= ( !empty($section['icon']) ? ' with-icon' : '' ) ?>">
 
   <div class="wrapper frame">
 
-    <? if ( $section['icon'] ) { ?>
+    <? if ( !empty($section['icon']) ) { ?>
       <div class="icon">
         <img src="<?= $section['icon']['sizes']['thumbnail'] ?>" alt="Point of Pride" />
       </div>
     <? } ?>
 
-    <? if ( $section['text'] ) { ?>
+    <? if ( !empty($section['text']) ) { ?>
       <div class="column col-12 text text-center featured-quote">
         <blockquote class="quote">
           <?= $section['text'] ?>
         </blockquote>
+        <? if ( !empty($section['attribution']) ) { ?>
+          <div class="attribution italic">
+            <?= $section['attribution'] ?>
+          </div>
+        <? } ?>
       </div>
     <? } ?>
 

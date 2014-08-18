888d252423145d6a51462e181f512f5e1443eb70
August W. Miller <hello@gusmiller.com>
2014-08-17 19:48:48 -0700

    Message in archive template when no posts are found

--- wp-content/themes/scripted/partials/archive-loop.php
+++ wp-content/themes/scripted/partials/archive-loop.php
@@ -2,4 +2,6 @@
   <? while ( have_posts() ) { the_post(); ?>
     <? include get_embed( get_post_type() ); ?>
   <? } ?>
+<? } else { ?>
+  Looks like we haven't added anything here, yet. Check back soon!
 <? } ?>

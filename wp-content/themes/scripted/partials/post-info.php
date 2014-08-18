0cb1875dc7b1152383f9353223eee2cd4740faf7
August W. Miller <hello@gusmiller.com>
2014-08-17 20:09:18 -0700

    Post info includes link to archive

--- wp-content/themes/scripted/partials/post-info.php
+++ wp-content/themes/scripted/partials/post-info.php
@@ -1,6 +1,6 @@
 <div class="post-info small text-grey-dark">
-  <div class="type float-left">
-    <?= se_post_nicename($post) ?>
+  <div class="type float-left link-grey">
+    <a href="<?= get_post_type_archive_link($post->post_type) ?>"><?= se_post_nicename($post) ?></a>
   </div>
   <div class="date italic float-right">
     <?= get_the_time('F j, Y') ?>

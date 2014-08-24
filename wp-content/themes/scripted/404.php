886029d9cbcf416c501d6afb23ee063e215c6ac0
savannahjulian <savannah.julian@gmail.com>
2014-08-23 20:41:54 -0700

    Fixing 404 layout

--- wp-content/themes/scripted/404.php
+++ wp-content/themes/scripted/404.php
@@ -4,7 +4,7 @@
 
 <section class="error-404">
   <div class="wrapper">
-    <div class="column col-8 push-2 tablet-three-quarters mobile-full">
+    <div class="column col-8 centered tablet-three-quarters mobile-full">
       <p>
         We couldn&rsquo;t figure out exactly what you were looking for. The <a href="<?= home_url() ?>">homepage</a> is a great starting point if you&rsquo;d like to learn about <?= bloginfo('title') ?>. If you still can&rsquo;t find what you&rsquo;re looking for, please <a href="#">get in touch</a>.
       </p>

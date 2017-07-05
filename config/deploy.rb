51e4bbefec1134f34daa9cddccd6406e00ca8afa
August Miller <hello@gusmiller.com>
2017-07-05 12:01:05 -0700

    Deploy config updates based on new application location

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,10 +27,10 @@ set :log_level, :info
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('wp-config.php', '.htaccess')
+set :linked_files, fetch(:linked_files, []).push('web/wp-config.php', 'web/.htaccess')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('wp-content/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('web/wp-content/uploads')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
